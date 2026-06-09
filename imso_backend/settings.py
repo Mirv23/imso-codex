d1962ddb550737894c4d12158df254b8e52e8ecb
Mirv23 <mirvpaulletang@gmail.com>
2026-06-08 22:23:09 -0400

    fix: deploiement Vercel - Whitenoise sans manifest, load static dans tous les partials

--- imso_backend/settings.py
+++ imso_backend/settings.py
@@ -108,9 +108,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+WHITENOISE_USE_FINDERS = True
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     }
 }
 
